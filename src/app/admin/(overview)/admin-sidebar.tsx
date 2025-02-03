7f0953dbc7150b5d813b89c65feb1835429f4dc4
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2025-02-04 00:36:46 +0100

    fix: lock file

--- src/app/admin/(overview)/admin-sidebar.tsx
+++ src/app/admin/(overview)/admin-sidebar.tsx
@@ -32,7 +32,7 @@ export function AdminSidebarar({ user, ...props }: AppSidebarProps) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                   <Command className="size-4" />
                 </div>
 
