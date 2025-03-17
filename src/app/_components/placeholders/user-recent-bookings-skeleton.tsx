f938c7ef1a2b071b8b06232ad5cd92bae3bea389
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2025-03-17 21:05:27 +0100

    style: user dashboard skeletons

--- src/app/_components/placeholders/user-recent-bookings-skeleton.tsx
+++ src/app/_components/placeholders/user-recent-bookings-skeleton.tsx
@@ -1,20 +1,10 @@
-import { SquarePlusIcon } from "lucide-react";
-
 export default function UserRecentBookingsSkeleton() {
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-4">
-      <div className="flex h-[268px] animate-pulse items-center justify-center rounded-xl border border-dashed">
-        <SquarePlusIcon className="size-5 text-muted-foreground" />
-      </div>
-      <div className="flex h-[268px] animate-pulse items-center justify-center rounded-xl border border-dashed">
-        <SquarePlusIcon className="size-5 text-muted-foreground" />
-      </div>
-      <div className="flex h-[268px] animate-pulse items-center justify-center rounded-xl border border-dashed">
-        <SquarePlusIcon className="size-5 text-muted-foreground" />
-      </div>
-      <div className="flex h-[268px] animate-pulse items-center justify-center rounded-xl border border-dashed">
-        <SquarePlusIcon className="size-5 text-muted-foreground" />
-      </div>
+      <div className="flex h-[268px] animate-pulse items-center justify-center rounded-xl bg-muted"></div>
+      <div className="flex h-[268px] animate-pulse items-center justify-center rounded-xl bg-muted"></div>
+      <div className="flex h-[268px] animate-pulse items-center justify-center rounded-xl bg-muted"></div>
+      <div className="flex h-[268px] animate-pulse items-center justify-center rounded-xl bg-muted"></div>
     </div>
   );
 }
--- src/app/_components/placeholders/user-upcoming-lessons-skeleton.tsx
+++ src/app/_components/placeholders/user-upcoming-lessons-skeleton.tsx
@@ -1,14 +1,8 @@
-import { BookOpenIcon } from "lucide-react";
-
 export default function UserUpcomingLessonsSkeleton() {
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-      <div className="flex h-[525px] animate-pulse items-center justify-center rounded-xl border border-dashed">
-        <BookOpenIcon className="size-5 text-muted-foreground" />
-      </div>
-      <div className="flex h-[525px] animate-pulse items-center justify-center rounded-xl border border-dashed">
-        <BookOpenIcon className="size-5 text-muted-foreground" />
-      </div>
+      <div className="flex h-[525px] animate-pulse items-center justify-center rounded-xl bg-muted"></div>
+      <div className="flex h-[525px] animate-pulse items-center justify-center rounded-xl bg-muted"></div>
     </div>
   );
 }
